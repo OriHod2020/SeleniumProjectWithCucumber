ed888ee8526ea9a44d5e3bd8bea5069982cb9ec4
Ori Hod <orihod2020@gmail.com>
2020-10-18 15:36:15 +0300

    Updating the pom.xml file and renamed Runner class file to TestRunner to resolve errors

--- src/test/java/com/tutorialsninja/automation/runner/TestRunner.java
+++ src/test/java/com/tutorialsninja/automation/runner/TestRunner.java
@@ -13,6 +13,6 @@ import cucumber.api.junit.Cucumber;
 glue= {"classpath:com.tutorialsninja.automation/stepdef"},
 plugin= {"html:target/cucumber_html_report"})
 //tags= {"@Register", "@One"})
-public class Runner {
+public class TestRunner {
 
 }
