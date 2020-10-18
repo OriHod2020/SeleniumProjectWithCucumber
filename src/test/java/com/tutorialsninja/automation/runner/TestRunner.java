ff11327457eb4ee25c3df3899e7d543541cd4788
Ori Hod <orihod2020@gmail.com>
2020-10-18 18:15:19 +0300

    Updated Project

--- src/test/java/com/tutorialsninja/automation/runner/TestRunner.java
+++ src/test/java/com/tutorialsninja/automation/runner/TestRunner.java
@@ -6,13 +6,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"classpath:FeatureFiles/Register.feature", 
-		"classpath:FeatureFiles/Login.feature" ,
-		"classpath:FeatureFiles/search.feature",
-		"classpath:FeatureFiles/Orders.feature"},
+@CucumberOptions(features={"classpath:FeatureFiles/Register.feature"}, 
+	//	"classpath:FeatureFiles/Login.feature" ,
+	//	"classpath:FeatureFiles/search.feature",
+	//	"classpath:FeatureFiles/Orders.feature"},
 glue= {"classpath:com.tutorialsninja.automation/stepdef"},
-plugin= {"html:target/cucumber_html_report"})
-//tags= {"@Register", "@One"})
+plugin= {"html:target/cucumber_html_report"},
+tags= {"@Register", "@Two"})
 public class TestRunner {
 
 }
